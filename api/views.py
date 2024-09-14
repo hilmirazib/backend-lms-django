0dc7f242fc0619aaeb6d2f7cd995749910e7e654
hilmi razib yusuf <hilmiyusuf197@gmail.com>
2024-09-14 13:14:53 +0700

    setup password change

--- api/views.py
+++ api/views.py
@@ -4,9 +4,10 @@ from api import serializer as api_serializer
 from userauths.models import User, Profile
 
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
 
 import random
 
@@ -63,3 +64,22 @@ class PasswordResetEmailVerifyAPIView(generics.RetrieveAPIView):
 
             print("link ======", link)
         return user
+
+class PasswordChangeAPIView(generics.CreateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = api_serializer.UserSerializer
+    
+    def create(self, request, *args, **kwargs):
+        otp = request.data['otp']
+        uuidb64 = request.data['uuidb64']
+        password = request.data['password']
+        
+        user = User.objects.get(id=uuidb64, otp=otp)
+        if user:
+            user.set_password(password)
+            # user.otp = ""
+            user.save()
+
+            return Response({"message": "Password Changed Successfully"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": "User Does Not Exists"}, status=status.HTTP_404_NOT_FOUND)
